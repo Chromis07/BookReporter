a58011e482ad66ef1a80d02000677043d4be7152
chromis07 <chromis07@gmail.com>
2021-08-12 01:06:55 +0900

    모델생성

--- django_BR/bookreporter/models.py
+++ django_BR/bookreporter/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+# 장르
+class Genre(models.Model):
+    name = models.CharField(max_length = 20)
+
+
+# 책 정보
+class Book(models.Model):
+    title = models.CharField(max_length = 30, db_index=True)
+    author = models.CharField(max_length = 20)
+    publisher = models.CharField(max_length = 20)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    image = models.ImageField()
+    book_url = models.URLField()
+    desc = models.TextField()
+    # published_date 적으려면?? (날짜 형식)
+
+
+# 책 리뷰
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    message = models.TextField()
+
+
+# 책 인용구
+class Quote(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    phrase = models.TextField()
+
