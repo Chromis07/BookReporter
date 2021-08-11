6505e265e4d214fac9b9686f918cd5a0ed83f4de
chromis07 <chromis07@gmail.com>
2021-08-12 00:15:45 +0900

    add urlpatterns for bookreporters app

--- django_BR/django_BR/urls.py
+++ django_BR/django_BR/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookreporter/', include('bookreporter.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
