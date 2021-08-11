f653604923d84e3ca3f80685b2ab0beefc52cf75
chromis07 <chromis07@gmail.com>
2021-08-12 03:48:10 +0900

    view 구성, templates 기본 구성

--- django_BR/bookreporter/views.py
+++ django_BR/bookreporter/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect, render, resolve_url
 
-# Create your views here.
+from bookreporter.models import Book, Review, Quote
+
+def book_list(request):
+    qs = Book.objects.all()
+    return render(request, "bookreporter/book_list.html", {
+        "book_list" : qs,
+    })
+    
+
+def book_detail(request, pk):
+    book = Book.objects.all(pk=pk)
+    return render(request, "bookreporter/book_detail.html", {
+        "book" : book,
+    })
+    
+    
+    
+# review 쓰기 + quote 쓰기
+
+def review_new():
+    pass
+
+def review_edit():
+    pass
+
+def review_delete():
+    pass
+
+def quote_new():
+    pass
+
+def quote_edit():
+    pass
+
+def quote_delete():
+    pass
